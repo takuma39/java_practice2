374a4faf2f67343539e283dc7be941ac15ced1af
takuma39 <takuma.sato.3939@gmail.com>
2020-11-22 00:54:50 +0900

    section3_30

---
+++ java_project1_section3/src/java_project1_section3/Sample.java
@@ -0,0 +1,20 @@
+package java_project1_section3;
+
+public class Sample {
+/*
+ * コメント
+ */
+
+/** Javadoc形式
+ * author: Takuma Sato
+ * copyright:
+ * @param args
+ */
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HEllo Eclips");
+	}
+
+}
+
---
+++ java_project1_section3/src/java_project1_section3/Staffinfo.java
@@ -0,0 +1,18 @@
+package java_project1_section3;
+
+class Staff {
+	String name;
+	int staffid;
+	String email;
+}
+public class Staffinfo {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Staff sato = new Staff();
+		sato.name = "Takuma Sato";
+
+		System.out.println(sato.name);
+	}
+
+}

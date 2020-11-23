cd2928d2a2dcdd44a57cccfd65cd8eca31255190
takuma39 <takuma.sato.3939@gmail.com>
2020-11-23 18:08:47 +0900

    section4_41

---
+++ java_project1_section4/src/java_project1_section4/calc.java
@@ -0,0 +1,17 @@
+package java_project1_section4;
+
+public class calc {
+
+	public static void main(String[] args) {
+		// 算術演算子　+ - * %
+		int a = 15;
+		int b = 4;
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		// %はあまりの数 15÷4=3 余り3
+		System.out.println("a % b = " + (a % b));
+	}
+
+}

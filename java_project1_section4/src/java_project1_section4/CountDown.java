0008aa15563872ce634b8fd39c8aec34014b4a4a
takuma39 <takuma.sato.3939@gmail.com>
2020-11-23 21:39:03 +0900

    section4_47

---
+++ java_project1_section4/src/java_project1_section4/CountDown.java
@@ -0,0 +1,12 @@
+package java_project1_section4;
+
+public class CountDown {
+
+	public static void main(String[] args) {
+		for (int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ java_project1_section4/src/java_project1_section4/CountUp.java
@@ -0,0 +1,12 @@
+package java_project1_section4;
+
+public class CountUp {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}

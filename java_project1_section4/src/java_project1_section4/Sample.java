fe62584865ff32ecd0fee7dd9eadfec5a9e0f2d5
takuma39 <takuma.sato.3939@gmail.com>
2020-11-23 17:50:11 +0900

    section4_39

---
+++ java_project1_section4/src/java_project1_section4/Sample.java
@@ -0,0 +1,15 @@
+package java_project1_section4;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// \nで改行　\などを「エスケープ文字」という
+		System.out.println("Hello Eclips!\nIt's Tuesday today.");
+
+		System.out.println("-------------------------------------");
+		// \"あああ\"　で" " /  \'あああ\’　で' '
+		System.out.println("Hello Eclips!\n\'It's Tuesday today.\'\n\"今日は火曜日です\"");
+	}
+
+}

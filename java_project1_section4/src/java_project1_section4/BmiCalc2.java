8cc1ec3dbee419055b316b8be48c78c077143f93
takuma39 <takuma.sato.3939@gmail.com>
2020-11-23 21:32:15 +0900

    section4_46

---
+++ java_project1_section4/src/java_project1_section4/BmiCalc2.java
@@ -0,0 +1,30 @@
+package java_project1_section4;
+import java.util.Scanner;
+
+public class BmiCalc2 {
+/* BMI（体格指数を計算するプログラム）
+ * BMI = 体重 / （身長 * 身長）
+ * 体重 kg / 身長 cm
+ */
+	public static void main(String[] args) {
+		double weight, height, bmi;
+
+		//weight = 65;
+		//height = 172;
+
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("体重は？ (kg) :");
+		weight = stdIn.nextDouble();
+
+		System.out.println("身長は？ (cm) :");
+		height = stdIn.nextDouble();
+
+		height /=  100;
+
+		bmi = weight / (height * height);
+
+		System.out.println("BMI: " + bmi + "です。");
+
+	}
+
+}

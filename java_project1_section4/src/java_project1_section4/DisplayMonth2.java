b007da6d5e1c7aa672223dc0588eab3aea39f59f
takuma39 <takuma.sato.3939@gmail.com>
2020-11-24 02:47:16 +0900

    section4_55

---
+++ java_project1_section4/src/java_project1_section4/DisplayMonth2.java
@@ -0,0 +1,26 @@
+package java_project1_section4;
+
+public class DisplayMonth2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String[] month = {
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December"
+		};
+		for (int i = 0; i < 12; i++) {
+			System.out.println(month[i]);
+		}
+	}
+
+}

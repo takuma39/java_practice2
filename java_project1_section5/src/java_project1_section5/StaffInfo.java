404614d7ba81e7f53a3bf67bf4f35c98d7af58c2
takuma39 <takuma.sato.3939@gmail.com>
2020-11-24 20:38:21 +0900

    section5_58

--- java_project1_section5/src/java_project1_section5/StaffInfo.java
+++ java_project1_section5/src/java_project1_section5/StaffInfo.java
@@ -10,13 +10,41 @@ class Staff {
 		//thisはsayhello（メソッド）を定義しているクラス
 	}
 
+	// ソース→フィールドを使用してコンストラクターを生成
 	public Staff(String name, int staffid, String email) {
 		super();
 		this.name = name; //nameは引数 String nameに入力したもの
 		this.staffid = staffid;
 		this.email = email;
 	}
+
+	// ソース→getterおよびsetterの生成
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStaffid() {
+		return staffid;
+	}
+
+	public void setStaffid(int staffid) {
+		this.staffid = staffid;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
+
+// ------------------------------------------------------
 
 public class StaffInfo {
 
@@ -25,7 +53,12 @@ public class StaffInfo {
 		Staff sato = new Staff("Takuma Sato", 1234, "test@test");
 		//sato.name = "Takuma Sato";
 
-		sato.sayhello();
+		//sato.sayhello();
 		//System.out.println(sato.name);
+
+		System.out.println("【社員情報】");
+		System.out.println("氏名：" + sato.getName());
+		System.out.println("社員番号：" + sato.getStaffid());
+		System.out.println("email：" + sato.getEmail());
 	}
 }

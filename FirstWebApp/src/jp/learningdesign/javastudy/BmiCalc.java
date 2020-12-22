8784ead82f0d11f07c1f611d9e9c288c17e549de
takuma39 <takuma.sato.3939@gmail.com>
2020-12-22 23:25:43 +0900

    section6_73

--- FirstWebApp/src/jp/learningdesign/javastudy/BmiCalc.java
+++ FirstWebApp/src/jp/learningdesign/javastudy/BmiCalc.java
@@ -42,7 +42,21 @@ public class BmiCalc extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String height_in,weight_in;
+		double height, weight, bmi;
+		height_in = request.getParameter("height1");
+		weight_in = request.getParameter("weight1");
+		height = Double.parseDouble(height_in);
+		weight = Double.parseDouble(weight_in);
+		height /= 100;
+		bmi = weight / (height * height);
+
+		out.println("<html><body>");
+		out.println("あなたのBMI、体格指数は" + bmi + "です。");
+		out.println("</body></html>");
 	}
 
 }
